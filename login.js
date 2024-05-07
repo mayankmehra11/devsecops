f1f5ef885fb1a7d50c1b1cef9bb215f8e3a64216
mayankmehra11 <50366440+mayankmehra11@users.noreply.github.com>
2024-05-07 15:56:12 +0530

    Delete login.js

--- login.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const username = document.getElementById("username").value;
-    localStorage.setItem("username", username);
-    window.location.href = "welcome.html";
-});
