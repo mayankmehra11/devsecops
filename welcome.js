85abf751deb8ab425acec7c13705d0627cc4936d
mayankmehra11 <50366440+mayankmehra11@users.noreply.github.com>
2024-05-07 15:56:21 +0530

    Delete welcome.js

--- welcome.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const username = localStorage.getItem("username");
-    if (username) {
-        document.getElementById("welcomeMessage").innerText = "Welcome, " + username + "!";
-    } else {
-        document.getElementById("welcomeMessage").innerText = "Welcome!";
-    }
-});
